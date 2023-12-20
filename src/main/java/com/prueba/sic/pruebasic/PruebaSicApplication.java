1d54cbeb4ca99cad4f730f3961b3a0d3a30feff0
Jhonnathan0314 <jffc314@gmail.com>
2023-12-20 08:43:45 -0500

    Empty Spring Boot project

---
+++ src/main/java/com/prueba/sic/pruebasic/PruebaSicApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.sic.pruebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/sic/pruebasic/PruebaSicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.sic.pruebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
